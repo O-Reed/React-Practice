90c749f8be5472398ccd6b5819a1a3deaac0edf2
O-Reed <cooldev0822@gmail.com>
2025-01-08 21:29:02 -0500

    Anthony_RenderingList_Using map

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import UserProfile from "./components/UserProfile"
+import UserDetails from "./components/UserDetails";
+// import UserProfile from "./components/UserProfile";
 function App() {
+  // const callMe = () => {
+  //   console.log("hello");
+  // }
 
-  const callMe = () => {
-    console.log("hello");
-  }
+  const mockUsers = [
+    {
+      id: 1,
+      username: "Anthony",
+      email: "Anthony@dev.com",
+    },
+    {
+      id: 2,
+      username: "Favoroso",
+      email: "Favoroso@dev.com",
+    },
+  ];
+
   return (
     <div>
-      <h1>React Component</h1>
-      <UserProfile username="Anthony" age={36} isLoggined={false} favoriteFoods={[
-        {
-          name: "Sushi",
-          id: "sushi",
-        },
-        {
-          name: "Pizza",
-          id: "pizza",
-        },
-      ]} callMe={callMe} />
-    </div>  
-  )
+      {mockUsers.map((mockUser) => {
+        return <UserDetails mockUser={mockUser} key={mockUser.id} />;
+      })}
+    </div>
+    // <div>
+    //   <h1>React Component</h1>
+    //   <UserProfile username="Anthony" age={36} isLoggined={false} favoriteFoods={[
+    //     {
+    //       name: "Sushi",
+    //       id: "sushi",
+    //     },
+    //     {
+    //       name: "Pizza",
+    //       id: "pizza",
+    //     },
+    //   ]} callMe={callMe} />
+    // </div>
+  );
 }
 
-export default App
+export default App;
