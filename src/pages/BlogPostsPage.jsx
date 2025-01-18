9a2f1a72bbac6fd40b9ebc700f892fb049b63589
O-Reed <cooldev0822@gmail.com>
2025-01-18 16:24:16 -0500

    Anthony_React_Router_navigator

--- src/pages/BlogPostsPage.jsx
+++ src/pages/BlogPostsPage.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 const BlogPostsPage = () => {
-  return <div>BlogPostsPage</div>;
+  const { state } = useLocation();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if (state && state.posts) {
+      setPosts(state.posts);
+    }
+  }, [state]);
+
+  return (
+    <div>
+      <h1>Welcome to Posts page</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <section>
+            <p>{post.content}</p>
+          </section>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BlogPostsPage;
